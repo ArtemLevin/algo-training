b2408e9a0ff0c017183d8193892b5fe979f267b6
ArtemLevin <python_study_2022@mail.ru>
2023-03-21 11:14:04 +0300

    printing GCD for two numbers

--- gcd.py
+++ gcd.py
@@ -2,7 +2,11 @@ numbers = input("Enter numbers t find GCD ")
 a, b = list(map(int, numbers.split()))
 print(f'{a = }, {b = }')
 
-def gcd(a, b):
-    return gcd(b, a%b) if b else a
 
-gcd(a, b)
+def gcd(a, b):
+    return gcd(b, a % b) if b else a
+
+
+print(f'GCD is {gcd(a, b)}')
+
+

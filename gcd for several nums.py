7613ce57297a99b0260acc63bf3b09489365b2af
ArtemLevin <python_study_2022@mail.ru>
2023-03-21 11:27:30 +0300

    Final cut gcd for several nums

--- gcd for several nums.py
+++ gcd for several nums.py
@@ -2,8 +2,10 @@ numbers = input("Enter numbers to find GCD ")
 num_list = list(map(int, numbers.split()))
 print(*num_list)
 
+
 def gcd(a, b):
     return gcd(b, a % b) if b else a
+
 
 def gcd_for_several_nums(num_list):
     result = 0
@@ -11,5 +13,5 @@ def gcd_for_several_nums(num_list):
         result = gcd(result, item)
     return result
 
-print(gcd_for_several_nums(num_list))
 
+print(gcd_for_several_nums(num_list))

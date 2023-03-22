8a3f78c02146a19023236ca5188e2a7c294e1bc6
ArtemLevin <python_study_2022@mail.ru>
2023-03-22 23:21:34 +0300

    GCD 10.11.5

---
+++ gcd_10.11.5.py
@@ -0,0 +1,30 @@
+dots = input("Enter coordinates of dots: ")
+xa, ya, xb, yb = list(map(int, dots.split(" ")))
+x_left = min(xa, xb)
+x_right = max(xa, xb)
+y_top = max(ya, yb)
+y_bottom = min(ya, yb)
+
+
+print(x_left, x_right, y_top, y_bottom)
+
+x_length = abs(x_right - x_left)
+y_length = abs(y_bottom - y_top)
+
+print(x_length, y_length)
+
+k = (ya - yb)/(xa - xb)
+b = ya - k*xa
+counter = 0
+
+for i in range(x_length+1):
+        x = x_left + i
+        y = 0
+        for j in range(y_length+1):
+            y = y_bottom + j
+            result = k*x + b
+            if y == result:
+                print(x, y)
+                counter = counter + 1
+
+print(counter)

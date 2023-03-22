5e96537e3a46a1079ac875bea638eba6486dc6e7
ArtemLevin <python_study_2022@mail.ru>
2023-03-22 23:36:35 +0300

    New way to find dots

--- gcd_10.11.5.py
+++ gcd_10.11.5.py
@@ -5,7 +5,6 @@ x_right = max(xa, xb)
 y_top = max(ya, yb)
 y_bottom = min(ya, yb)
 
-
 print(x_left, x_right, y_top, y_bottom)
 
 x_length = abs(x_right - x_left)
@@ -13,18 +12,31 @@ y_length = abs(y_bottom - y_top)
 
 print(x_length, y_length)
 
-k = (ya - yb)/(xa - xb)
-b = ya - k*xa
+k = (ya - yb) / (xa - xb)
+b = ya - k * xa
 counter = 0
 
-for i in range(x_length+1):
-        x = x_left + i
-        y = 0
-        for j in range(y_length+1):
-            y = y_bottom + j
-            result = k*x + b
-            if y == result:
-                print(x, y)
-                counter = counter + 1
+for i in range(x_length + 1):
+    x = x_left + i
+    y = 0
+    for j in range(y_length + 1):
+        y = y_bottom + j
+        result = k * x + b
+        if y == result:
+            print(x, y)
+            counter = counter + 1
 
-print(counter)
+print(counter, '\n')
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def dots(x_length, y_length):
+    return 1 + gcd(x_length, y_length)
+
+
+print(dots(x_length, y_length))
